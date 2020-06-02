07eb7b7ae8a383ebc0c656ecaf570faf3d8ee525
ArubaruZERO <63741440+ArubaruZERO@users.noreply.github.com>
2020-06-02 14:22:33 +0200

    Delete app.js

--- generador aleatorio/css/css/app.js
+++
@@ -1,8 +0,0 @@
-// Generador aleatorio
-
-var max = 100 , min = 0;
-var range = max - min + 1 
-
-var randomPick = (n, min ,max) => Math.floor(Math.random() * range) + min;
-
-console.log(randomPick(1, 0, 30))
